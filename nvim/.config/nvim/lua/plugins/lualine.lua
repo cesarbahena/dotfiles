90fda917a2b0156963094fe8bb4b69c619110e23
cesarbahena <cabahenac@gmail.com>
2025-09-15 12:33:59 -0600

    feat(nvim): show rightmost window filetype icon in lualine tabline

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -93,9 +93,37 @@ return {
       },
       lualine_z = {
         {
-          'filetype',
-          colored = true,
-          icon_only = true,
+          function()
+            local windows = vim.api.nvim_tabpage_list_wins(0)
+            local normal_windows = {}
+            for _, win in ipairs(windows) do
+              if vim.api.nvim_win_get_config(win).relative == '' then
+                table.insert(normal_windows, win)
+              end
+            end
+            
+            if #normal_windows == 0 then 
+              return require('mini.icons').get('filetype', vim.bo.filetype)
+            end
+            
+            -- Get the rightmost window of the top row
+            local top_row_y = vim.api.nvim_win_get_position(normal_windows[1])[1]
+            local rightmost_win = normal_windows[1]
+            local rightmost_x = vim.api.nvim_win_get_position(normal_windows[1])[2]
+            
+            for _, win in ipairs(normal_windows) do
+              local pos = vim.api.nvim_win_get_position(win)
+              if pos[1] == top_row_y and pos[2] > rightmost_x then
+                rightmost_win = win
+                rightmost_x = pos[2]
+              end
+            end
+            
+            local buf = vim.api.nvim_win_get_buf(rightmost_win)
+            local filetype = vim.api.nvim_get_option_value('filetype', { buf = buf })
+            local icon, hl = require('mini.icons').get('filetype', filetype)
+            return icon
+          end,
           padding = { right = 0 },
         },
         {
