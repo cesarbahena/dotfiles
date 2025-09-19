96e6774e99393251c8f508a85ba44b38a5ee052a
cesarbahena <cabahenac@gmail.com>
2025-09-19 15:53:35 -0600

    refactor(harpoon): more sensible keybinds

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,31 +9,23 @@ return {
 
     -- Trigger heirline update when harpoon list changes
     harpoon:extend {
-      ADD = function() 
-        vim.api.nvim_exec_autocmds('BufEnter', {}) 
-      end,
-      REMOVE = function() 
-        vim.api.nvim_exec_autocmds('BufEnter', {}) 
-      end,
-      REORDER = function()
-        vim.api.nvim_exec_autocmds('BufEnter', {}) 
-      end,
-      LIST_CHANGE = function()
-        vim.api.nvim_exec_autocmds('BufEnter', {}) 
-      end,
+      ADD = function() vim.api.nvim_exec_autocmds('BufEnter', {}) end,
+      REMOVE = function() vim.api.nvim_exec_autocmds('BufEnter', {}) end,
+      REORDER = function() vim.api.nvim_exec_autocmds('BufEnter', {}) end,
+      LIST_CHANGE = function() vim.api.nvim_exec_autocmds('BufEnter', {}) end,
     }
   end,
   keys = {
-    motion { 'Harpoon 1', function() require('harpoon'):list():select(1) end },
-    motion { 'Harpoon 2', function() require('harpoon'):list():select(2) end },
-    motion { 'Harpoon 3', function() require('harpoon'):list():select(3) end },
-    motion { 'Harpoon 4', function() require('harpoon'):list():select(4) end },
+    motion { 'harpoon 1', function() require('harpoon'):list():select(1) end },
+    motion { 'harpoon 2', function() require('harpoon'):list():select(2) end },
+    motion { 'harpoon 3', function() require('harpoon'):list():select(3) end },
+    motion { 'harpoon 4', function() require('harpoon'):list():select(4) end },
     motion {
-      'Harpoon menu',
+      'harpoon Menu',
       function() require('harpoon').ui:toggle_quick_menu(require('harpoon'):list()) end,
     },
     motion {
-      'Harpoon add',
+      'harpoon Mark file',
       function() require('harpoon'):list():add() end,
     },
   },
