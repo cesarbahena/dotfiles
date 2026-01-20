7a67d5767916c535594d5cfd60e4d075fee264bc
cesarbahena <cabahenac@gmail.com>
2026-01-20 17:54:15 -0600

    feat(nvim): add treesitter installer

--- editor/.config/nvim/lua/packages.lua
+++ editor/.config/nvim/lua/packages.lua
@@ -8,4 +8,6 @@ vim.pack.add {
   gh 'nvim-mini/mini.pick',
   gh 'nvim-mini/mini.files',
   gh 'mfussenegger/nvim-dap',
+  gh 'nvim-treesitter/nvim-treesitter',
+  gh 'theHamsta/nvim-dap-virtual-text',
 }
--- editor/.config/nvim/plugin/dap.lua
+++ editor/.config/nvim/plugin/dap.lua
@@ -25,7 +25,16 @@ map('n', '<F10>', dap.step_over, q)
 map('n', '<F11>', dap.step_into, q)
 map('n', '<F12>', dap.step_out, q)
 map('n', '<leader>b', dap.toggle_breakpoint, q)
+map('n', '<leader>dr', dap.repl.toggle, q)
 map('n', '<leader>B', function()
   dap.set_breakpoint(vim.fn.input 'Condition: ')
 end, q)
-map('n', '<leader>dr', dap.repl.toggle, q)
+
+require('nvim-dap-virtual-text').setup {
+  enabled = true,
+  enabled_commands = true,
+  highlight_changed_variables = true,
+  highlight_new_as_changed = true,
+  show_stop_reason = true,
+  commented = false,
+}
