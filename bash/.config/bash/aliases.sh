70cfbad07b94d5af1415a3ee208850ce60f06090
cesarbahena <cabahenac@gmail.com>
2026-01-26 16:12:54 -0600

    feat(bash): add portable aliases and fix folder structure for proper stow of bin

---
+++ bash/.config/bash/aliases.sh
@@ -0,0 +1,11 @@
+# Core utils sensible defaults
+alias l='ls -nohaX --group-directories-first --color'
+alias ls='ls -A --color'
+alias cp='cp -rv'
+alias rm='rm -rv'
+alias mkdir='mkdir -pv'
+alias path='"$PATH" | tr : "\n"'
+alias du='dust 2>/dev/null' # silence permission errors
+alias g='git status -b --show-stash'
+alias v='nvim'
+
